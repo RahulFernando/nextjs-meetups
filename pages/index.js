049e8e4925221437ef0958d789aa038282311f8d
RahulFernando <rahulrulz680@gmail.com>
2022-06-02 15:30:43 +0530

    head

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
+import Head from "next/head";
 import { MongoClient } from "mongodb";
 
 // component
 import MeetupList from "../components/meetups/MeetupList";
 
 const HomePage = (props) => {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <>
+      <Head>
+        <title>React MeetUp</title>
+        <meta name="description" content="Browse huge list of highly active react meetups!" />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
 };
 
 // export async function getServerSideProps(context) {
